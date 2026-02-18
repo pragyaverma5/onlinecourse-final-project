a789a4577221237c7f333a136927a7c871612b41
Pragya Verma <162286680+pragyaverma5@users.noreply.github.com>
2026-02-19 00:04:34 +0530

    Update admin.py

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,9 +1,48 @@
 from django.contrib import admin
-from .models import Course, Lesson, Enrollment, Question, Choice, Submission
+from .models import Course, Lesson, Enrollment, Question, Choice, Submission, Instructor
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionInline(admin.TabularInline):
+    model = Question
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+
+class LessonAdmin(admin.ModelAdmin):
+    inlines = [QuestionInline]
 
 admin.site.register(Course)
-admin.site.register(Lesson)
+admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Enrollment)
-admin.site.register(Question)
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
 admin.site.register(Submission)
+admin.site.register(Instructor)
+from django.contrib import admin
+from .models import Course, Lesson, Enrollment, Question, Choice, Submission, Instructor
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionInline(admin.TabularInline):
+    model = Question
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+
+class LessonAdmin(admin.ModelAdmin):
+    inlines = [QuestionInline]
+
+admin.site.register(Course)
+admin.site.register(Lesson, LessonAdmin)
+admin.site.register(Enrollment)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
+admin.site.register(Submission)
+admin.site.register(Instructor)
